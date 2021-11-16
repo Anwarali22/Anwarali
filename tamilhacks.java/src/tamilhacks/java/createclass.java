65294d86bc4c43bd5903033f3392b9066271cccd
Anwar <Anwar@DESKTOP-Q80UIVG>
2021-11-16 08:00:41 +0530

    first commit

---
+++ tamilhacks.java/src/tamilhacks/java/createclass.java
@@ -0,0 +1,16 @@
+package tamilhacks.java;
+public class newclass{
+	static void methodone() {
+ System.out.print(" hello");	
+ return ;
+	}
+	}
+public class createclass {
+	public static void main(String[]args) {
+		
+		newclass object = new newclass(); 
+     	object.methodone();
+		System.out.println("object");
+	}
+
+}
